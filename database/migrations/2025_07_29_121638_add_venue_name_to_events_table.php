36f9b76ec5cddf59fdc71428fdb7d50c93fade5b
mohammad didarul islam <didarul1981@gmail.com>
2025-08-10 01:37:20 +0600

    testing image problem

--- database/migrations/2025_07_29_121638_add_venue_name_to_events_table.php
+++ database/migrations/2025_07_29_121638_add_venue_name_to_events_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->dropColumn('venue_name');
+            $table->dropColumn('venue_name');  
         });
     }
 };
